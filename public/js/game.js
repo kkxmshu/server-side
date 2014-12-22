c0256f8096ecb0bb8854bf9f02e5998762de7e45
Maxim <kkxmshu@gmail.com>
2014-12-22 23:36:32 +0200

    Drawing with Bezier curves

--- public/js/game.js
+++ public/js/game.js
@@ -3,6 +3,8 @@ var spriteRotation = 0;
 var line = 10;
 var offset = line / 2;
 
+var lineColor = "black";
+
 var w = 0;
 var h = 0;
 
@@ -11,12 +13,12 @@ var cell_h = 0;
 var cell_w = 0;
 
 function initGame() {
-	mySprite = loadSprite("src/blocks.png");
+	// mySprite = loadSprite("src/blocks.png");
 	w = canvas.width;
 	h = canvas.height;
 
-	cell_h = (h-line) / 3;
-	cell_w = (w-line) / 3;
+	cell_h = (h-line*3) / 3;
+	cell_w = (w-line*3) / 3;
 }
 
 function updateScene(delta) {
@@ -39,9 +41,6 @@ function loadSprite(imagePath) {
 }
 
 function drawSprite(imageObject, x, y, sx, sy, ds, rotation, scale) {
-	// var w = imageObject.width;
-	// var h = imageObject.height;
-
 	ctx.save();
 	ctx.translate(x, y);
 	ctx.rotate(rotation);
@@ -55,7 +54,7 @@ var UP = 1000, DOWN = 1001, LEFT = 1010, RIGHT = 1100;
 
 var maze = {
 	ways: {
-		left: 2, right: 5, top: 4, bottom: 8
+		left: 2, right: 5, top: 1, bottom: 9
 	},
 	init: function() {
 		// get ways from server 
@@ -69,40 +68,52 @@ var maze = {
 }
 
 
-function wall(sx, sy, dx, dy, color) {
+function wall(sx, sy, dx, dy, color, isVertical) {
+	var c1x = sx + Math.floor((Math.random() * 10) - 0), 
+		c1y = sy + Math.floor((Math.random() * 10) - 0),
+		c2x = dx - Math.floor((Math.random() * 200) - 0),
+		c2y = dy + Math.floor((Math.random() * 10) - 0);
+	if(isVertical) {
+		c1x = sx + Math.floor((Math.random() * 5) - 2); 
+		c1y = sy + Math.floor((Math.random() * 100) - 0);
+		c2x = dx - Math.floor((Math.random() * 5) - 5);
+		c2y = dy + Math.floor((Math.random() * 25) - 0);
+	} 
+	
 	ctx.save();
 	ctx.beginPath();
 	ctx.strokeStyle = color;
 	ctx.lineWidth = line;
-	ctx.lineCap = "square";
-	ctx.moveTo(sx + offset, sy + offset);
-	ctx.lineTo(dx + offset, dy + offset);
-	ctx.stroke();
+	ctx.lineCap = "round";
 
+	ctx.moveTo(sx + offset, sy + offset);
+	ctx.bezierCurveTo(c1x, c1y, c2x, c2y, dx + offset, dy + offset);
+
+	ctx.stroke();
 	ctx.restore();
 }
 var BOTTOM = 1, RIGHT = 2, TOP = 4, LEFT = 8;
 function drawCell(sx, sy, cell) {
 	if((cell & BOTTOM) != 0) {
-		wall(sx, sy + cell_h, sx + cell_w, sy + cell_h, "red");
+		wall(sx, sy + cell_h, sx + cell_w, sy + cell_h, lineColor, false);
 	} else {
 		// bottom blur
 	}
 
 	if((cell & RIGHT) != 0) {
-		wall(sx + cell_w, sy, sx + cell_w, sy + cell_h, "red");
+		wall(sx + cell_w, sy, sx + cell_w, sy + cell_h, lineColor, true);
 	} else {
 		// right blur
 	}
 
 	if((cell & TOP) != 0) {
-		wall(sx, sy, sx + cell_w, sy, "red");
+		wall(sx, sy, sx + cell_w, sy, lineColor, false);
 	} else {
 		// top blur
 	}
 
 	if((cell & LEFT) != 0) {
-		wall(sx, sy, sx, sy + cell_h, "red");
+		wall(sx, sy, sx, sy + cell_h, lineColor, true);
 	} else {
 		// left blur
 	}
