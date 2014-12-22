784e85e9d7bc9f2c600948544b7d86109e29d047
Maxim <kkxmshu@gmail.com>
2014-12-23 00:38:28 +0200

    Added docs

--- public/js/game.js
+++ public/js/game.js
@@ -1,19 +1,24 @@
-var mySprite;
-var spriteRotation = 0;
+// Line width
 var line = 10;
 var offset = line / 2;
 
+// Wall color
 var lineColor = "black";
 
+// Width of canvas
 var w = 0;
+// Height of canvas
 var h = 0;
 
-
+// Cell height
 var cell_h = 0;
+// Cell width
 var cell_w = 0;
 
+/**
+ * Calculates cells width and height
+ */
 function initGame() {
-	// mySprite = loadSprite("src/blocks.png");
 	w = canvas.width;
 	h = canvas.height;
 
@@ -21,25 +26,41 @@ function initGame() {
 	cell_w = (w-line*3) / 3;
 }
 
-function updateScene(delta) {
-	spriteRotation += (Math.PI / 180.0) * 45 * delta;
-}
-
+/**
+ * Redraw canvas
+ * 
+ * @param argument for changing values
+ */
 function renderScene(delta) {
-	var offset = line / 2;
-
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.fillRect((w / 2)-32, (h / 2)-32, 64, 64);
 	maze.draw();
 
 }
-
+/**
+ * Method for loading sprite
+ * 
+ * @param path to sprite
+ * @returns object with loaded sprite 
+ */
 function loadSprite(imagePath) {
 	var image = new Image();
 	image.src = imagePath;
 	return image;
 }
 
+/**
+ * Draw sprite
+ *
+ * @param image, which need to draw
+ * @param x-coordinate
+ * @param y-coordinate
+ * @param x-coordinate of sprite (1st point)
+ * @param y-coordinate of sprite (1st point)
+ * @param height/width for cutting the sprite from 1st point
+ * @param rotation value in radians
+ * @param scale value
+ */
 function drawSprite(imageObject, x, y, sx, sy, ds, rotation, scale) {
 	ctx.save();
 	ctx.translate(x, y);
@@ -49,16 +70,24 @@ function drawSprite(imageObject, x, y, sx, sy, ds, rotation, scale) {
 	ctx.restore();
 }
 
+// Variables of move direction
 var UP = 1000, DOWN = 1001, LEFT = 1010, RIGHT = 1100;
 
-
+// Maze object
 var maze = {
+	// Cells values
 	ways: {
 		left: 2, right: 5, top: 1, bottom: 9
 	},
+	/**
+	 * Refresh cells values
+	 */
 	init: function() {
-		// get ways from server 
+		
 	},
+	/**
+	 * Draw cells
+	 */
 	draw: function() {	
 		drawCell(cell_w, 0, this.ways.top);
 		drawCell(cell_w, cell_h * 2, this.ways.bottom);
@@ -67,8 +96,18 @@ var maze = {
 	}
 }
 
-
+/**
+ * Draw one wall
+ *
+ * @param source x
+ * @param source y 
+ * @param destination x
+ * @param destination y
+ * @param color of the wall
+ * @param is this wall vertical? 
+ */
 function wall(sx, sy, dx, dy, color, isVertical) {
+	// Calculate values of Bezier control points
 	var c1x = sx + Math.floor((Math.random() * 10) - 0), 
 		c1y = sy + Math.floor((Math.random() * 10) - 0),
 		c2x = dx - Math.floor((Math.random() * 200) - 0),
@@ -92,7 +131,17 @@ function wall(sx, sy, dx, dy, color, isVertical) {
 	ctx.stroke();
 	ctx.restore();
 }
+
+// Direction values for bitmask
 var BOTTOM = 1, RIGHT = 2, TOP = 4, LEFT = 8;
+
+/**
+ * Draw walls of cell
+ *
+ * @param source x
+ * @param source y
+ * @param cell
+ */
 function drawCell(sx, sy, cell) {
 	if((cell & BOTTOM) != 0) {
 		wall(sx, sy + cell_h, sx + cell_w, sy + cell_h, lineColor, false);
@@ -119,11 +168,18 @@ function drawCell(sx, sy, cell) {
 	}
 }
 
+// Player object
 var player = {
 	direction: DOWN,
 	scale: 10,
-	update: function() {	},
-	draw: function() {	},
+	update: function() {},
+	draw: function() {},
+
+	/**
+	 * One step in the direction of
+	 *
+	 * @param move direction 
+	 */
 	goto: function(direction) {
 		switch(direction) {
 			case UP:
@@ -150,6 +206,11 @@ var player = {
 	}
 }
 
+/**
+ * Send move direction to the server
+ *
+ * @param direction
+ */
 function toServer(direction) {
 
 }
